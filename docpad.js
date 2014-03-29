503752c8562d2e9757c0b606cf2f69964dfa5679
Michael Williams <dinosaur@riseup.net>
2014-03-28 17:23:42 -0700

    port to docpad

---
+++ docpad.js
@@ -0,0 +1,66 @@
+// http://docpad.org/docs/config
+
+require('longjohn');
+
+module.exports = {
+  templateData: {
+    site: {
+      title: "Subliminal Spaces",
+      description: "Creating Spaces That Inspire",
+      styles: ["/styles/index.css"],
+      scripts: ["/scripts/bundle.js"],
+    },
+  },
+  detectEncoding: true,
+  plugins: {
+    handlebars: {
+      helpers: {
+        partial: function (content, options) {
+          return this.partial(content, options);
+        },
+        block: function (blockName) {
+          return this.getBlock(blockName).toHTML();
+        },
+      },
+    },
+    browserifybundles: {
+      bundles: [{
+        arguments: ['-t', 'uglifyify'],
+        entry: 'scripts/index.js',
+        out: 'scripts/bundle.js',
+      }],
+      environments: {
+        development: {
+          bundles: [{
+            arguments: ['-d'],
+            entry: 'scripts/index.js',
+            out: 'scripts/bundle.js',
+          }],
+        },
+      },
+    },
+    raw: {
+      'font-awesome': {
+        command: ['rsync', '-r', 'node_modules/font-awesome/fonts/', 'out/fonts'],
+      },
+      semantic: {
+        command: ['rsync', '-r', 'node_modules/semantic/src/fonts/', 'out/fonts'],
+      },
+    },
+    ghpages: {
+      deployRemote: 'origin',
+      deployBranch: 'gh-pages',
+    },
+    menu: {
+      menuOptions: {
+        optimize: false,
+        skipFiles: /^(scripts|styles)/,
+      },
+    },
+  },
+  environments: {
+    development: {
+      port: 5000,
+    },
+  },
+};
---
+++ src/documents/index.html.hb
@@ -0,0 +1,10 @@
+---
+layout: "default"
+title: "Home"
+styles:
+  - "http://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,500,800,900"
+  - "http://fonts.googleapis.com/css?family=The+Girl+Next+Door"
+---
+<div class="slider container">
+  <p>{{ site.description }}</p>
+</div>
